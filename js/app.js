9b27da32eb51495ec15286b38984544415aee1fe
Tristan Challis <tristan.challis@live.co.uk>
2017-10-21 10:17:11 +0100

    Working Angular routing and basic form

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-//Creating a new angular module, taking app name and injecting
-//the dependency arrays ngRoute and RouteControllers
 angular.module('KHApp', ['ngRoute', 'RouteControllers']);
 
 angular.module('KHApp').config(function($locationProvider, $routeProvider) {
-	$locationProvider.html5Mode(true); //Enable href routing without hashes
-
+	$locationProvider.html5Mode(true);
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
 	})
-	.when('/accounts/newsletter', {
+	.when('/newsletter', {
 		templateUrl: 'templates/newsletter.html',
 		controller: 'newsletterController'
 	});
--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,15 @@
 angular.module('RouteControllers', [])
 	.controller('HomeController', function($scope) {
 		$scope.title = "Testing for kingdom hearts news";
+	})
+	.controller('newsletterController', function($scope){
+		$scope.signupUser = {};
+
+		$scope.submitForm = function() {
+			if($scope.signupForm.$valid){
+				$scope.signupUser.username = $scope.user.username;
+				$scope.signupUser.password = $scope.user.password;
+			}
+			console.log($scope.signupUser.username + " " + $scope.signupUser.password);
+		}
 	});
