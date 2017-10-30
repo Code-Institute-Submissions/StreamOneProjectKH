27bf5d74ed874d8a56fb11d3e7257e92b1fb5a32
Tristan Challis <tristan.challis@live.co.uk>
2017-10-30 11:52:40 +0000

    Filling content, adding functionality

--- js/controller.js
+++ js/controller.js
@@ -9,8 +9,12 @@ angular.module('RouteControllers', [])
 			if($scope.signupForm.$valid){
 				$scope.signupUser.username = $scope.user.username;
 				$scope.signupUser.password = $scope.user.password;
+				$scope.signupUser.email = $scope.user.email;
 			}
-			console.log($scope.signupUser.username + " " + $scope.signupUser.password);
+			console.log($scope.signupUser.username + " " + $scope.signupUser.password + " " + $scope.signupUser.email);
+		}
+		$scope.submitted = function(){
+			alert("sick one " + $scope.signupUser.username);
 		}
 	})
 	.controller('charactersController', function($scope){
