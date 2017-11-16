01cfd5389f65254b7ab997f75e055aebe1ad72d6
Tristan Challis <tristan.challis@live.co.uk>
2017-11-16 10:52:30 +0000

    contact issues

--- js/controller.js
+++ js/controller.js
@@ -12,8 +12,7 @@ angular.module('RouteControllers', [])
 				$scope.signupUser.email = $scope.user.email;
 				$scope.signupUser.recieve = $scope.user.recieve;
 			}
-		}
-		$scope.submitted = function(){
+
 			if($scope.user.recieve){
 				alert("Thank you for selecting that you would like to contribute, we'd love for you to be a part of our team " + $scope.signupUser.firstName + "!");
 			}
@@ -21,6 +20,14 @@ angular.module('RouteControllers', [])
 				alert("Thank you for signing up " + $scope.signupUser.firstName + " we will notify you when new information comes out!");
 			}
 		}
+		// $scope.submitted = function(){
+		// 	if($scope.user.recieve){
+		// 		alert("Thank you for selecting that you would like to contribute, we'd love for you to be a part of our team " + $scope.signupUser.firstName + "!");
+		// 	}
+		// 	else{
+		// 		alert("Thank you for signing up " + $scope.signupUser.firstName + " we will notify you when new information comes out!");
+		// 	}
+		// }
 	})
 	.controller('charactersController', function($scope){
 
